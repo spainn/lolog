d679ae291ffe5a9ba28e2a64eb8ae994c1c191b5
spainn <spainn@users.noreply.github.com>
2025-07-31 22:59:50 -0500

    reset

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,3 @@
-// TODO
-// make readable from text file (some way to input workouts)
-// some way to define movements and workouts persistently
-//
-mod exercise;
-mod parser;
-
-use exercise::*;
-
 fn main() {
-    let movements = parser::parse_movements();
-    for m in movements {
-        println!("{}", m.name);
-    }
-
-    println!("\n\n\n");
-
-    parser::parse_complete_workout();
+    println!("test");
 }
