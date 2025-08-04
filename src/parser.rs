29c224b7b464e488bfc638dde6eab6780dbb9e4c
n <noahsemail12@gmail.com>
2025-08-03 23:12:05 -0500

    got the parser splitting up the text

--- src/parser.rs
+++ src/parser.rs
@@ -3,9 +3,13 @@ use crate::Muscle;
 
 use std::fs;
 
+// need a string to muscle macro to make sure that each muscle string is converted
+// into the correct enum variant
+
 pub fn parse_exercises() -> Vec<Exercise> {
 
     let file_path = "data/exercises.csv";
+    let mut exercises: Vec<Exercise> = vec!();
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file.");
@@ -18,11 +22,22 @@ pub fn parse_exercises() -> Vec<Exercise> {
     // TODOTODO convert to exercise and add to the returned vector
     for line in lines {
         println!("{line}");
+
+        let parts: Vec<&str> = line.split(",").collect();
+        exercises.push(
+            Exercise {
+                name: parts[0].to_string(),
+                main_mover: parts[1]
+            }
+        )
+
     }
 
     // temp exercise vector
-    vec!(Exercise {
-        name: String::from("bench press"),
-        main_mover: Muscle::Chest
-    })
+//    vec!(Exercise {
+//        name: String::from("bench press"),
+//        main_mover: Muscle::Chest
+//    })
+
+    return exercises;
 }
